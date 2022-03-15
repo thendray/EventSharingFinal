6ef8d675219b640f25476d7f94de0fb9ecd66216
nikitakuprin <nik.kuprin.02@mail.ru>
2022-03-15 11:57:57 +0300

    Add files

---
+++ EventSharing/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EventSharing.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSharing.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    }
+}
